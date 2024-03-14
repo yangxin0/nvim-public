e5ccfc4ab1b23de0dc6b9d83b7e441eebd4413ac
Xin Yang <yangxin0@outlook.com>
2024-03-14 15:51:51 +0800

    Update editor.lua as plugins list

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,71 +1,73 @@
 return {
-    "telescope.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make"
-        },
-        "nvim-telescope/telescope-file-browser.nvim"
-    },
-    keys = {
-        {
-            "\\\\",
-            function() require("telescope.builtin").find_files() end,
-            desc = "Search Files"
-        },
-        {
-            "<leader>b",
-            function() require("telescope.builtin").buffers() end,
-            desc = "Search Buffers"
-        },
-        {
-            "<leader>?",
-            function() require("telescope.builtin").oldfiles() end,
-            desc = "Recent Opened Files"
-        },
-        {
-            "<leader>f",
-            function()
-                local telescope = require("telescope")
-                local function telescope_buffer_dir()
-                    return vim.fn.expand("%:p:h")
-                end
-                telescope.extensions.file_browser.file_browser({
-                    path = "%:p:h",
-                    cwd = telescope_buffer_dir(),
-                    respect_ignore = false,
-                    hidden = true,
-                    disable_devicons = true,
-                    grouped = true,
-                    previewer = false,
-                    initial_mode = "normal",
-                    layout_config = { height = 40 }
-                })
-            end,
-            desc = "Open File Browser with the path of current buffer"
-        }
-    },
-    config = function(_, opts)
-        local telescope = require("telescope")
-        opts.pickers = {
-            find_files = {
-                theme = "dropdown",
-                previewer = false
+    {
+        "telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make"
             },
-            buffers = {
-                theme = "dropdown",
-                previewer = false
+            "nvim-telescope/telescope-file-browser.nvim"
+        },
+        keys = {
+            {
+                "\\\\",
+                function() require("telescope.builtin").find_files() end,
+                desc = "Search Files"
+            },
+            {
+                "<leader>b",
+                function() require("telescope.builtin").buffers() end,
+                desc = "Search Buffers"
+            },
+            {
+                "<leader>?",
+                function() require("telescope.builtin").oldfiles() end,
+                desc = "Recent Opened Files"
+            },
+            {
+                "<leader>f",
+                function()
+                    local telescope = require("telescope")
+                    local function telescope_buffer_dir()
+                        return vim.fn.expand("%:p:h")
+                    end
+                    telescope.extensions.file_browser.file_browser({
+                        path = "%:p:h",
+                        cwd = telescope_buffer_dir(),
+                        respect_ignore = false,
+                        hidden = true,
+                        disable_devicons = true,
+                        grouped = true,
+                        previewer = false,
+                        initial_mode = "normal",
+                        layout_config = { height = 40 }
+                    })
+                end,
+                desc = "Open File Browser with the path of current buffer"
             }
-        }
-        opts.extensions = {
-            file_browser = {
-                theme = "dropdown",
-                hijack_netrw = true
+        },
+        config = function(_, opts)
+            local telescope = require("telescope")
+            opts.pickers = {
+                find_files = {
+                    theme = "dropdown",
+                    previewer = false
+                },
+                buffers = {
+                    theme = "dropdown",
+                    previewer = false
+                }
             }
-        }
-        telescope.setup(opts)
-        require("telescope").load_extension("fzf")
-        require("telescope").load_extension("file_browser")
-    end
+            opts.extensions = {
+                file_browser = {
+                    theme = "dropdown",
+                    hijack_netrw = true
+                }
+            }
+            telescope.setup(opts)
+            require("telescope").load_extension("fzf")
+            require("telescope").load_extension("file_browser")
+        end
+    }
 }
