d69b353d5cd2f01af7f0e5028adc4d464757b7e7
Xin Yang <yangxin0@outlook.com>
2024-03-14 15:54:05 +0800

    Add jk-Escape keymap

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,14 @@
 return {
     {
+        "max397574/better-escape.nvim",
+        opts = {
+            mapping = { "jk" },
+            timeout = vim.o.timeoutlen,
+            clear_empty_lines = true,
+            keys = "<Esc>"
+        }
+    },
+    {
         "telescope.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
